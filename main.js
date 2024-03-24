9603cd402b91e1aaf30706cd8ae24f4726075c2f
unknown <asatryanliann@gmail.com>
2024-03-24 22:32:16 +0400

    coding style correction

--- main.js
+++ main.js
@@ -14,21 +14,27 @@ let currentPosition = 0;
 
 function createPagination() {
     const pagination = $('.pagination');
+
     for (let i = 0; i < slides.length; i++) {
         const dot = $('<span class="dot"></span>');
+
         dot.on('click', function() {
             goToSlide(i);
+
             $(slides[i]).css({
                 'backgroundColor': '#F5DDE3',
                 'transition': 'background-color 0.3s ease',
             });
+
             setTimeout(function () {
+
                 $(slides[i]).css({
                     'backgroundColor': '#EABEC3',
                     'transition': 'background-color 0.3s ease',
                 });
             }, 600);
         });
+
         pagination.append(dot);
     }
 }
@@ -38,15 +44,19 @@ function updatePagination(currentSlideIndex) {
     $('.dot').eq(currentSlideIndex).addClass('active');
 }
 
-// Function to go to a specific slide
 function goToSlide(slideIndex) {
+
     if(slideIndex == 0) {
+
         currentPosition = 0;
     } else if (slideIndex == slides.length - 1) {
+
         currentPosition = (slides.length - visibleSlides)*(slidesWidth + gap);
     } else {
+
         currentPosition = (slideIndex - 1) * (slidesWidth + gap);
     }
+
     $(slides[0]).stop().animate({ marginLeft: -currentPosition }, 'smooth');
     updatePagination(slideIndex);
     index = slideIndex;
@@ -88,17 +98,20 @@ slides.draggable({
     axis: 'x',
     containment: 'parent',
     cursor: 'move',
+
     start: function(event, ui) {
         $(this).addClass('dragging');
         ui.helper.css('width', slidesWidth); // Set width to maintain the slide width during drag
         $(this).data('startLeft', ui.position.left);
     },
+
     drag: function(event, ui) {
         let startLeft = $(this).data('startLeft');
         let mouseDelta = ui.position.left - startLeft;
         let currentIndex = $(this).index();
         goToSlide(currentIndex);
     },
+
     stop: function() {
         $(this).removeClass('dragging').removeAttr('style');
         slides.removeAttr('style'); // Reset styles after dragging
@@ -106,7 +119,6 @@ slides.draggable({
 });
 
 
-// Mouse events
 
 /*let isDragging = false;
 let startPosition;
@@ -125,9 +137,9 @@ slider.on('mousemove', e => {
         currentPos = e.clientX;
         currentTranslate = prevTranslate + currentPos - startPosition;
         if (currentTranslate > 0) {
-            currentTranslate = 0; // Prevent sliding to the left beyond the start
+            currentTranslate = 0;
         } else if (currentTranslate < -(slidesWidth * (slides.length - 2) + gap*(slides.length - 1) - sliderWidth)) {
-            currentTranslate = -(slidesWidth * (slides.length - 2) + gap*(slides.length - 1) - sliderWidth); // Prevent sliding to the right beyond the end
+            currentTranslate = -(slidesWidth * (slides.length - 2) + gap*(slides.length - 1) - sliderWidth);
         }
         slides.css('transform', `translateX(${currentTranslate}px)`);
     }
@@ -135,18 +147,22 @@ slider.on('mousemove', e => {
 
 slider.on('mouseup', () => {
 
-    if (!isDragging) return;
+    if (!isDragging) {
+        return;
+    }
+
     isDragging = false;
     const dragDistance = currentTranslate - prevTranslate;
+
     if (dragDistance < -100 && index < slides.length - 1) {
         index++;
     } else if (dragDistance > 100 && index > 0) {
         index--;
     }
+
     goToSlide(index);
 
     prevTranslate = currentTranslate;
-
     slides.css('cursor', 'auto');
 });*/
 
